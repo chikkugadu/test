6ed9f8a85677ef276147e130ab8ed41374833ec1
chikkugadu <58038474+chikkugadu@users.noreply.github.com>
2021-05-10 09:44:54 +0530

    initial commit

--- submit/urls.py
+++ submit/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from submit import views
 from .views  import add_detail
 from .views  import homepageview
-
+#new line for dev
 urlpatterns = [
     path('',views.homepageview,name='home'),
     path('add_detail/',views.add_detail,name='add_detail')
